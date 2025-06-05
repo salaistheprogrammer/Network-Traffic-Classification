78c59626eacd7a40a9d0887345d15bea1bac5bb7
Salai Saidhanyan M <67590261+salaistheprogrammer@users.noreply.github.com>
2025-06-05 15:08:11 +0530

    Update capture_live_packets.py

--- Classification/capture_live_packets.py
+++ Classification/capture_live_packets.py
@@ -6,9 +6,7 @@ from pathlib import Path
 from ext_features_preprocess import extract_features
 
 
-model = joblib.load("path/to/your_trained_model.pkl")
-
-
+model = joblib.load("path\to\model.pkl")
 
 def handle_packet(packet):
     
@@ -16,25 +14,18 @@ def handle_packet(packet):
 
     if features:
         df = pd.DataFrame([features])
-
         categorical_columns = df.select_dtypes(include=['object', 'category']).columns.tolist()
         df = pd.get_dummies(df, columns=categorical_columns)
-        
         X_new = df.reindex(columns=categorical_columns, fill_value=0)
-
         prediction = model.predict(X_new)
-        
         df['Prediction'] = prediction
-        
         df.to_csv("live_predictions.csv", mode='a', header=not Path("live_predictions.csv").exists(), index=False)
 
 
 def capture_live_traffic(duration=300):
-    # Sniff packets for a certain duration
     sniff(prn=handle_packet, timeout=duration)
 
-# Main loop to run the capture, predict, and save process periodically
 while True:
-    capture_live_traffic(duration=300)  # Capture for 5 minutes
+    capture_live_traffic(duration=300)  
     print("Captured and predicted on live traffic. Waiting for the next round...")
-    time.sleep(60)  # Wait for 1 minute before the next round
+    time.sleep(60)  
