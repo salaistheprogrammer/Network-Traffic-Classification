8e5cfa6f21b08c05ca062712c434ed346510bb01
Salai Saidhanyan M <67590261+salaistheprogrammer@users.noreply.github.com>
2025-06-05 14:57:38 +0530

    Delete Classification/pcapngread.py

--- Classification/pcapngread.py
+++
@@ -1,5 +0,0 @@
-from pyshark import FileCapture
-
-packets3 = FileCapture("C:\SIH Classification\SIH Datasets\DDoS\PCAPs\packet-captures-main\\amp.TCP.syn.optionallyACK.optionallysamePort.pcapng")
-
-print(packet for packet in packets3)
