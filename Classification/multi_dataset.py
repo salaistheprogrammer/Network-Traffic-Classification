ea00d51ca754ff8e09e2ab6c30f2e8cdb8ef4ce3
Salai Saidhanyan M <67590261+salaistheprogrammer@users.noreply.github.com>
2025-06-05 14:58:28 +0530

    Update multi_dataset.py

--- Classification/multi_dataset.py
+++ Classification/multi_dataset.py
@@ -1,7 +1,4 @@
 from ext_features_preprocess import extract_features
-# from scapy.all import rdpcap
-# from pyshark import FileCapture
-
 
 def for_extract(packets_list: list):
 
@@ -12,36 +9,3 @@ def for_extract(packets_list: list):
         n += 1
         
     return feature
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# features = for_extract(packets)
-# print(features[0])
-
-# packets1 = rdpcap("C:\SIH Classification\SIH Datasets\DDoS\PCAPs\packet-captures-main\\amp.UDP.DNSANY.pcap")
-# packets2 = rdpcap("C:\SIH Classification\SIH Datasets\DDoS\PCAPs\packet-captures-main\\amp.dns.RRSIG.fragmented.pcap")
-# # packets3 = FileCapture("C:\SIH Classification\SIH Datasets\DDoS\PCAPs\packet-captures-main\\amp.TCP.syn.optionallyACK.optionallysamePort.pcapng")
-# # packets4 = rdpcap("C:\SIH Classification\SIH Datasets\DDoS\PCAPs\packet-captures-main\\amp.UDP.bacnet.IOT.37810.pcapng")
-# packets5 = rdpcap("C:\SIH Classification\SIH Datasets\DDoS\PCAPs\packet-captures-main\\amp.UDP.IOT.port37810.JSON.pcap")
-
-# packets = [packets1, packets2, packets5]
