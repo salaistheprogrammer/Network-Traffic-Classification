fa697b3c7792d2b37df05f27a91b4ae98e9779d4
Salai Saidhanyan M <67590261+salaistheprogrammer@users.noreply.github.com>
2025-06-05 15:01:46 +0530

    Update categorical_columns_sc.py

--- Classification/categorical_columns_sc.py
+++ Classification/categorical_columns_sc.py
@@ -10,13 +10,9 @@ def select_categorical_columns(df: pd.DataFrame) -> list:
     for col in possible_categorical:
         if not pd.api.types.is_numeric_dtype(df[col]):
             sample_value = df[col].dropna().iloc[0] 
-            if not sample_value.isdigit():  # Exclude columns with purely numeric values
+            if not sample_value.isdigit():  
                 categorical_columns.append(col)
     categorical_columns.remove('Label') if 'Label' in categorical_columns else None
 
     return categorical_columns
 
-# Assuming df is your DataFrame
-# categorical_columns = select_categorical_columns(df)
-
-# print("Categorical columns:", categorical_columns)
